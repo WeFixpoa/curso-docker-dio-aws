23733beca9eba12361100143a4eb1fe7e83061f7
WeFixpoa <att@wefixit.inf.br>
2022-11-21 15:28:19 -0300

    update data

--- 04 Definição e Criação de um Docker File/app.go
+++ 04 Definição e Criação de um Docker File/app.go
@@ -1,11 +1,12 @@
 package main
+
 import (
-    "fmt"
+	"fmt"
 )
 
 func main() {
-  fmt.Println("Qual é o seu nome:? ")
-  var name string
-  fmt.Scanln(&name)
-  fmt.Printf("Oi, %s! Eu sou a linguagem Go! ", name)
+	fmt.Println("Qual é o seu nome:? ")
+	var name string
+	fmt.Scanln(&name)
+	fmt.Printf("Oi, %s! Eu sou a linguagem Go! ", name)
 }
